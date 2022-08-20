e1991552968553a618231d6281884ba0035b3f66
asicsforthemasses <asicsforthemasses@gmail.com>
2022-08-20 16:44:14 +0200

    Added QLAPlacer to the placer task

--- gui/tasks/place.cpp
+++ gui/tasks/place.cpp
@@ -13,7 +13,30 @@ void Tasks::Place::execute(GUI::Database &database, ProgressCallback callback)
         return;
     }    
 
-    std::this_thread::sleep_for (std::chrono::seconds(6));
+    if (!topModule->m_netlist)
+    {
+        error("Top module has no netlist\n");
+        return;        
+    }
+
+    // FIXME: for now use the first region to place the top module
+    auto regionIter = database.floorplan()->begin();
+    if (regionIter == database.floorplan()->end())
+    {
+        error("No regions defined\n");
+        return;
+    }
+
+    auto regionKeyPair = *regionIter;
+
+    bool ok = LunaCore::QLAPlacer::place(*regionKeyPair.rawPtr(), 
+        *topModule->m_netlist.get(), nullptr);
+
+    if (!ok)
+    {
+        error("Placement failed!\n");
+        return;
+    }
 
     done();
 }
