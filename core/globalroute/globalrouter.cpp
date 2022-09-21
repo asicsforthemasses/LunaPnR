5eb924ff745f6098b31f892b73e85220c9dbaa4a
asicsforthemasses <asicsforthemasses@gmail.com>
2022-09-21 14:25:49 +0200

    WIP

---
+++ core/globalroute/globalrouter.cpp
@@ -0,0 +1,4 @@
+#include "globalrouter.h"
+
+using namespace LunaCore;
+
---
+++ core/globalroute/globalrouter.h
@@ -0,0 +1,19 @@
+// SPDX-FileCopyrightText: 2021-2022 Niels Moseley <asicsforthemasses@gmail.com>
+//
+// SPDX-License-Identifier: GPL-3.0-only
+
+#pragma once
+
+namespace LunaCore::GlobalRouter
+{
+
+class Router
+{
+public:
+    Router() = default;
+    
+protected:
+
+};
+
+};
