ebc3884a606eafd36b859edd71c06a9a6ded29d3
asicsforthemasses <asicsforthemasses@gmail.com>
2023-08-06 03:02:51 +0200

    .

---
+++ gui/widgets/layerwidget.cpp
@@ -0,0 +1,16 @@
+// SPDX-FileCopyrightText: 2021-2023 Niels Moseley <asicsforthemasses@gmail.com>
+//
+// SPDX-License-Identifier: GPL-3.0-only
+
+#include "layerwidget.h"
+
+namespace GUI
+{
+
+LayerWidget::LayerWidget(QWidget *parent) : QTableView(parent)
+{
+    setSelectionBehavior(QTableView::SelectRows);
+    setSelectionMode(QAbstractItemView::SingleSelection);
+}
+
+};
---
+++ gui/widgets/layerwidget.h
@@ -0,0 +1,23 @@
+// SPDX-FileCopyrightText: 2021-2023 Niels Moseley <asicsforthemasses@gmail.com>
+//
+// SPDX-License-Identifier: GPL-3.0-only
+
+#pragma once
+#include "lunacore.h"
+
+#include <QTableView>
+
+/** List of layers with a specific ordering.
+    layers can be enabled/disabled by the user
+*/
+namespace GUI
+{
+
+class LayerWidget : public QTableView
+{
+public:
+    LayerWidget(QWidget *parent = nullptr);
+
+};
+
+};
