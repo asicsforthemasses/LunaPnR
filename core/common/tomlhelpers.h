80ce03f3016e0412cd8e40039c948a61b5759e47
asicsforthemasses <asicsforthemasses@gmail.com>
2023-10-26 22:38:48 +0200

    Added TOML helpers

---
+++ core/common/tomlhelpers.h
@@ -0,0 +1,78 @@
+#pragma once
+#include <type_traits>
+#include <toml++/toml.h>
+
+namespace ChipDB::TOML
+{
+
+template <typename T>
+concept SimpleType = std::is_integral_v<T> || std::is_same_v<T, std::string> 
+    || std::is_same_v<T, const std::string>
+    || std::is_same_v<T, bool>;
+
+void insert(toml::table &tbl, const std::string &name, 
+    SimpleType auto const& value)
+{
+    tbl.insert(name, value);
+}
+
+template<typename T>
+void insert(toml::table &tbl, const std::string &arrayName, 
+    const std::vector<T> &array)
+{
+    toml::array arr;
+    arr.insert(arr.begin(), array.begin(), array.end());
+    tbl.insert(arrayName, arr);
+}
+
+bool retrieve(toml::table &tbl, const std::string &name, SimpleType auto &result)
+{
+    using valueType = std::remove_reference_t<decltype(result)>;
+    auto ptr = tbl[name].value<valueType>();
+
+    if (!ptr)
+    {
+        if constexpr (std::is_same_v<valueType, std::string>)
+        {
+            result.clear();
+        }
+        
+        if constexpr (std::is_same_v<valueType, bool>)
+        {
+            result = false;
+        }
+
+        if constexpr (std::is_integral_v<valueType>)
+        {
+            result = 0;
+        }   
+
+        return false;     
+    }
+
+    result = ptr.value();
+    return true;
+}
+
+template<typename T>
+bool retrieve(toml::table &tbl, const std::string &arrayName, std::vector<T> &result)
+{
+    result.clear();
+
+    auto arrPtr = tbl[arrayName].as_array();
+    if (arrPtr != nullptr)
+    {
+        arrPtr->for_each(
+            [&](toml::value<T> &item)
+            {
+                result.emplace_back(item);
+            }
+        );
+
+        return true;
+    }
+
+    return false;
+}
+
+};
