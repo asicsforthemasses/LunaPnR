3215ab1a136740f275b43b58023cdba1393700cc
asicsforthemasses <asicsforthemasses@gmail.com>
2024-03-10 21:24:16 +0100

    added missing database.h

---
+++ core/database/database.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "dbtypes.h"
+#include "observer.h"
+#include "namedstorage.h"
+#include "net.h"
+#include "instance.h"
+#include "netlist.h"
+#include "enums.h"
+#include "netlisttools.h"
+#include "techlib.h"
+#include "floorplan.h"
+#include "design.h"
