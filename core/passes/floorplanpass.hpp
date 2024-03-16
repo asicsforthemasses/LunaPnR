56b3a93e368bd0b23e4cf69f8f85d7b3ac237940
asicsforthemasses <asicsforthemasses@gmail.com>
2024-03-16 22:30:47 +0100

    Update SPDX headers

--- core/passes/floorplanpass.hpp
+++ core/passes/floorplanpass.hpp
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: 2021-2024 Niels Moseley <asicsforthemasses@gmail.com>
 //
 // SPDX-License-Identifier: GPL-3.0-only
+
 #pragma once
 #include "common/logging.h"
 #include "pass.hpp"
